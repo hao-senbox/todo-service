0513126c3df72a0cd6ae8e99836cc49aa9722368
HaoVo2007 <hao01638081724@gmail.com>
2025-08-29 16:01:57 +0700

    push code

--- internal/todo/service.go
+++ internal/todo/service.go
@@ -81,10 +81,6 @@ func (s *todoService) CreateTodo(ctx context.Context, req CreateTodoRequest, use
 		return nil, fmt.Errorf("invalid due_date format, expected YYYY-MM-DD HH:MM:SS: %v", err)
 	}
 
-	if req.ImageTask == "" {
-		return nil, fmt.Errorf("image_task is required")
-	}
-
 	if !req.Urgent {
 		return nil, fmt.Errorf("urgent is required")
 	}
